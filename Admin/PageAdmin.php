26ce10bbd39c1359299730fd05a045d4294750d0
Matteo Caberlotto <mcaber@gmail.com>
2013-03-15 15:25:52 +0100

    added template record update on page save; added custom template for page editing

--- Admin/PageAdmin.php
+++ Admin/PageAdmin.php
@@ -9,16 +9,22 @@ use Sonata\AdminBundle\Form\FormMapper;
 
 class PageAdmin extends Admin
 {
+    protected $container;
+
+    public function setContainer($container) {
+        $this->container = $container;
+    }
+
     protected function configureFormFields(FormMapper $formMapper)
     {
         $formMapper
                 ->add('name', null, array('required' => true))
                 ->add('layout', null, array('required' => true))
-                ->add('children', 'sonata_type_collection', array(), array(
-                    'edit' => 'inline',
-                    'inline' => 'table',
-                    'sortable'  => 'position'
-                ))
+//                ->add('children', 'sonata_type_collection', array(), array(
+//                    'edit' => 'inline',
+//                    'inline' => 'table',
+//                    'sortable'  => 'position'
+//                ))
                 ->add('route', null, array('required' => true))
         ;
     }
@@ -47,9 +53,39 @@ class PageAdmin extends Admin
         ;
     }
 
+    public function getTemplate($name)
+    {
+        switch ($name) {
+            case 'edit':
+                return 'RaindropPageBundle:Page:page_editor.html.twig';
+                break;
+            default:
+                return parent::getTemplate($name);
+                break;
+        }
+    }
+
     public function preUpdate($page)
     {
+        // make sure route points to proper controller
+        // @TODO: make this parameter configurable
         $page->setController('RaindropPageBundle:Page:index');
+
+        // update template last modified as the content has changed
+        $layout = $page->getLayout();
+        $orm = $this->container->get('doctrine.orm.default_entity_manager');
+        $templateClass = $this->container->getParameter('raindrop_twig_loader_bundle.entity_class');
+        $templateRepo = $orm
+            ->getRepository($templateClass);
+
+        // @TODO: fix this shit :)
+        $params = explode(':', $layout);
+        if ($params[0] == 'database') {
+            $tpl = $templateRepo->findOneByName($params[1]);
+            $tpl->setUpdated(new \DateTime);
+            $orm->persist($tpl);
+            $orm->flush();
+        }
     }
 
     public function postUpdate($page)
