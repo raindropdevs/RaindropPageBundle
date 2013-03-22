ca08e00ba4656aa1054735371cc638a72958c093
Matteo Caberlotto <mcaber@gmail.com>
2013-03-22 19:02:39 +0100

    overridden block edit action

--- Controller/BlockCRUDController.php
+++ Controller/BlockCRUDController.php
@@ -3,7 +3,91 @@
 namespace Raindrop\PageBundle\Controller;
 
 use Sonata\AdminBundle\Controller\CRUDController;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class BlockCRUDController extends CRUDController
 {
+    /**
+     * i have to extend whole controller and overwrite entire method. no good. - teo
+     * @param type $id
+     * @return type
+     * @throws NotFoundHttpException
+     * @throws AccessDeniedException
+     */
+    public function editAction($id = null)
+    {
+        // the key used to lookup the template
+        $templateKey = 'edit';
+
+        $id = $this->get('request')->get($this->admin->getIdParameter());
+
+        $object = $this->admin->getObject($id);
+
+        if (!$object) {
+            throw new NotFoundHttpException(sprintf('unable to find the object with id : %s', $id));
+        }
+
+        if (false === $this->admin->isGranted('EDIT', $object)) {
+            throw new AccessDeniedException();
+        }
+
+        $this->admin->setSubject($object);
+
+        /** @var $form \Symfony\Component\Form\Form */
+        $form = $this->admin->getForm();
+        $form->setData($object);
+
+        if ($this->get('request')->getMethod() == 'POST') {
+            $form->bind($this->get('request'));
+
+            $isFormValid = $form->isValid();
+
+            // persist if the form was valid and if in preview mode the preview was approved
+            if ($isFormValid && (!$this->isInPreviewMode() || $this->isPreviewApproved())) {
+                $this->admin->update($object);
+                $this->get('session')->setFlash('sonata_flash_success', 'flash_edit_success');
+
+                if ($this->isXmlHttpRequest()) {
+                    return $this->renderJson(array(
+                        'result'    => 'ok',
+                        'objectId'  => $this->admin->getNormalizedIdentifier($object)
+                    ));
+                }
+
+                // redirect to edit mode
+
+                // TEO: if we're coming from page admin, redirect to there
+                $referer = $this->get('request')->server->get('HTTP_REFERER');
+                if (preg_match('/\/admin\/raindrop\/page\/page\/[\d]+\/edit$/', $referer)) {
+//                    return $this->redirect('admin_raindrop_page_page_edit', array('id' =>  $object->getPage()));
+                    return $this->redirect($this->generateUrl('admin_raindrop_page_page_edit', array(
+                        'id' =>  $object->getPage()->getId(), 'layout' => true
+                    )));
+                }
+                return $this->redirectTo($object);
+            }
+
+            // show an error message if the form failed validation
+            if (!$isFormValid) {
+                if (!$this->isXmlHttpRequest()) {
+                    $this->get('session')->setFlash('sonata_flash_error', 'flash_edit_error');
+                }
+            } elseif ($this->isPreviewRequested()) {
+                // enable the preview template if the form was valid and preview was requested
+                $templateKey = 'preview';
+            }
+        }
+
+        $view = $form->createView();
+
+        // set the theme for the current Admin Form
+        $this->get('twig')->getExtension('form')->renderer->setTheme($view, $this->admin->getFormTheme());
+
+        return $this->render($this->admin->getTemplate($templateKey), array(
+            'action' => 'edit',
+            'form'   => $view,
+            'object' => $object,
+        ));
+    }
+
 }
