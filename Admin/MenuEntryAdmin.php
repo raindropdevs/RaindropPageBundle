ecfdc9f9ea4c89e2f9895ba1e39f68d6e3e2618e
Matteo Caberlotto <mcaber@gmail.com>
2013-05-08 12:45:06 +0200

    added file upload for menu entry

---
+++ Admin/MenuEntryAdmin.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Raindrop\PageBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Symfony\Component\DependencyInjection\Container;
+use Sonata\AdminBundle\Route\RouteCollection;
+
+class MenuEntryAdmin extends Admin
+{
+    protected $container;
+
+    public function setContainer($container)
+    {
+        $this->container = $container;
+    }
+
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $label = $this->getSubject()->getLabelString();
+
+        $formMapper
+            ->add('label', null, array(
+                'required' => false,
+                'data' => $label
+            ))
+            ->add('position', null, array('required' => false))
+            ->add('file', 'file', array('required' => false))
+        ;
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('label')
+        ;
+    }
+
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('label', null, array(
+                'template' => 'RaindropPageBundle:Menu:menu_admin_list_label.html.twig'
+            ))
+        ;
+    }
+
+    public function preUpdate($menuEntry)
+    {
+        $this->saveFile($menuEntry);
+    }
+
+    public function prePersist($menuEntry)
+    {
+        $this->saveFile($menuEntry);
+    }
+
+    public function saveFile($menuEntry) {
+        $appBasePath = dirname($this->container->getParameter('kernel.root_dir'));
+        $menuEntry->upload($appBasePath);
+    }
+}
--- Entity/MenuEntry.php
+++ Entity/MenuEntry.php
@@ -5,6 +5,7 @@ namespace Raindrop\PageBundle\Entity;
 use Doctrine\ORM\Mapping as ORM;
 use Raindrop\PageBundle\Entity\Page;
 use Raindrop\PageBundle\Entity\Menu;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 /**
  * @ORM\Entity(repositoryClass="Raindrop\PageBundle\Entity\PageMenuRepository")
@@ -44,6 +45,8 @@ class MenuEntry
      * @ORM\Column(nullable=true)
      */
     protected $label;
+
+    protected $file;
 
     /**
      * Get id
@@ -168,5 +171,98 @@ class MenuEntry
     public function getLabel()
     {
         return $this->label;
+    }
+
+    public function getLabelString() {
+        $label = $this->getLabel();
+        if (empty($label)) {
+            $label = $this->getPage()->getTitle();
+        }
+        return $label;
+    }
+
+    public function __toString() {
+        return (string) $this->getLabelString();
+    }
+
+    public function getAbsolutePath()
+    {
+        return null === $this->path
+            ? null
+            : $this->getUploadRootDir().'/'.$this->path;
+    }
+
+    public function getWebPath()
+    {
+        return null === $this->path
+            ? null
+            : $this->getUploadDir().'/'.$this->path;
+    }
+
+    protected function getUploadRootDir($appPath)
+    {
+        // the absolute directory path where uploaded
+        // documents should be saved
+        return $appPath . '/web/' . $this->getUploadDir();
+    }
+
+    protected function getUploadDir()
+    {
+        // get rid of the __DIR__ so it doesn't screw up
+        // when displaying uploaded doc/image in the view.
+        return 'uploads/documents';
+    }
+
+    public function upload($appPath)
+    {
+        // the file property can be empty if the field is not required
+        if (null === $this->getFile()) {
+            return;
+        }
+
+        // use the original file name here but you should
+        // sanitize it at least to avoid any security issues
+        $cleanName = $this->cleanFileName($this->getFile()->getClientOriginalName());
+
+        // move takes the target directory and then the
+        // target filename to move to
+        $this->getFile()->move(
+            $this->getUploadRootDir($appPath),
+            $cleanName
+        );
+
+        // set the path property to the filename where you've saved the file
+        $this->image = $cleanName;
+
+        // clean up the file property as you won't need it anymore
+        $this->file = null;
+    }
+
+    protected function cleanFileName() {
+        $name = $this->getFile()->getClientOriginalName();
+        $name = preg_replace('/[\s]+/', '_', $name);
+        $name = preg_replace('/[\-]+/', '-', $name);
+
+        return $name;
+    }
+
+        /**
+     * Sets file.
+     *
+     * @param UploadedFile $file
+     */
+    public function setFile(UploadedFile $file = null)
+    {
+        $this->file = $file;
+    }
+
+    /**
+     * Get file.
+     *
+     * @return UploadedFile
+     */
+    public function getFile()
+    {
+        return $this->file;
     }
 }
