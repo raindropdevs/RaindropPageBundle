4b8f91847463f256d25a00c873edff5beb91b01f
Edoardo Rivello <edoardo.rivello@gmail.com>
2013-05-24 15:53:19 +0200

    add createAction to page crud controller

--- Controller/PageCRUDController.php
+++ Controller/PageCRUDController.php
@@ -131,4 +131,82 @@ class PageCRUDController extends CRUDController
             'liip_theme_suffix' => $theme_suffix
         ));
     }
+
+    /**
+     * return the Response object associated to the create action
+     *
+     * @throws \Symfony\Component\Security\Core\Exception\AccessDeniedException
+     * @return Response
+     */
+    public function createAction()
+    {
+        // the key used to lookup the template
+        $templateKey = 'edit';
+
+        if (false === $this->admin->isGranted('CREATE')) {
+            throw new AccessDeniedException();
+        }
+
+        $object = $this->admin->getNewInstance();
+
+        $this->admin->setSubject($object);
+
+        /** @var $form \Symfony\Component\Form\Form */
+        $form = $this->admin->getForm();
+        $form->setData($object);
+
+        if ($this->get('request')->getMethod() == 'POST') {
+            $form->bind($this->get('request'));
+
+            $isFormValid = $form->isValid();
+
+            // persist if the form was valid and if in preview mode the preview was approved
+            if ($isFormValid && (!$this->isInPreviewMode() || $this->isPreviewApproved())) {
+                $this->admin->create($object);
+
+                if ($this->isXmlHttpRequest()) {
+                    return $this->renderJson(array(
+                        'result' => 'ok',
+                        'objectId' => $this->admin->getNormalizedIdentifier($object)
+                    ));
+                }
+
+                $this->get('session')->setFlash('sonata_flash_success','flash_create_success');
+                // redirect to edit mode
+                return $this->redirectTo($object);
+            }
+
+            // show an error message if the form failed validation
+            if (!$isFormValid) {
+                if (!$this->isXmlHttpRequest()) {
+                    $this->get('session')->setFlash('sonata_flash_error', 'flash_create_error');
+                }
+            } elseif ($this->isPreviewRequested()) {
+                // pick the preview template if the form was valid and preview was requested
+                $templateKey = 'preview';
+            }
+        }
+
+        $view = $form->createView();
+
+        // set the theme for the current Admin Form
+        $this->get('twig')->getExtension('form')->renderer->setTheme($view, $this->admin->getFormTheme());
+
+        $useTheme = $this->container->getParameter('use_liip_theme');
+        $theme = $this->get('request')->get('theme');
+        if (is_null($theme)) {
+            $theme_suffix = '';
+        } else {
+            $theme_suffix = '|' . $theme;
+        }
+
+        return $this->render($this->admin->getTemplate($templateKey), array(
+            'action' => 'create',
+            'form'   => $view,
+            'object' => $object,
+            'use_liip_theme' => $useTheme,
+            'liip_theme' => $theme,
+            'liip_theme_suffix' => $theme_suffix
+        ));
+    }
 }
