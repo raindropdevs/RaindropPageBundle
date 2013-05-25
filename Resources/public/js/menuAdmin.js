97dbd27cb953352611401af0ae9373e504c74942
Matteo Caberlotto <mcaber@gmail.com>
2013-05-25 20:53:20 +0200

    menu admin: UI/UX

--- Resources/public/js/menuAdmin.js
+++ Resources/public/js/menuAdmin.js
@@ -45,7 +45,8 @@ var menuAdmin = (function () {
                     cancel: "a.ui-icon", // clicking an icon won't initiate dragging
                     revert: "invalid", // when not dropped, the item will revert back to its initial position
                     containment: "document",
-                    helper: "clone"
+                    helper: "clone",
+                    appendTo: '.tabbable'
                 });
 
             $(".droppable")
