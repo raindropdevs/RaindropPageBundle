263fc743063b15443d16e37bdd7ff15825e8323e
Matteo Caberlotto <mcaber@gmail.com>
2013-06-15 15:07:51 +0200

    removed source pages from menu editing

--- Controller/MenuCRUDController.php
+++ Controller/MenuCRUDController.php
@@ -80,9 +80,9 @@ class MenuCRUDController extends CRUDController
         $this->get('twig')->getExtension('form')->renderer->setTheme($view, $this->admin->getFormTheme());
 
         // the sources
-        $pages = $this->container
-            ->get('raindrop_page.page.manager')
-            ->getCountryPages($object->getCountry());
+//        $pages = $this->container
+//            ->get('raindrop_page.page.manager')
+//            ->getCountryPages($object->getCountry());
 
         // the actual menu
         $menuPages = $this
@@ -93,7 +93,7 @@ class MenuCRUDController extends CRUDController
             'action' => 'edit',
             'form'   => $view,
             'object' => $object,
-            'pages' => $pages,
+//            'pages' => $pages,
             'menu_pages' => $this
                 ->get('raindrop_page.directory_tree')
                 ->buildTree($menuPages)
