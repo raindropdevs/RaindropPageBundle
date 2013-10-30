cc597a452c83d20b353b1f77546168ac9e9fed06
Enrico Antonello <eantonello@h-art.com>
2013-10-30 18:04:31 +0100

    update listener

--- Listener/UpdateListener.php
+++ Listener/UpdateListener.php
@@ -4,11 +4,14 @@ namespace Raindrop\PageBundle\Listener;
 
 use Doctrine\ORM\Event\LifecycleEventArgs;
 use Raindrop\PageBundle\Entity\BlockVariable;
+use Raindrop\PageBundle\Entity\Block;
+
 /**
  * UpdateListener
  */
 class UpdateListener
 {
+    //post update for block variables, blocks
     public function postUpdate(LifecycleEventArgs $args)
     {
         $entity = $args->getEntity();
@@ -20,6 +23,37 @@ class UpdateListener
             $entity->getBlock()->getPage()->setUpdated(new \DateTime);   
             $entityManager->persist($entity->getBlock()->getPage());
             $entityManager->flush();
+        }
+       
+        if ($entity instanceof Block) {
+            if($entity->getPage()) {
+                $entity->getPage()->setUpdated(new \DateTime);   
+                $entityManager->persist($entity->getPage());
+                $entityManager->flush();                
+            }
+        }        
+    }
+    
+    //post persist for block variables, blocks
+    public function postPersist(LifecycleEventArgs $args)
+    {
+        $entity = $args->getEntity();
+        $entityManager = $args->getEntityManager();
+        
+        if ($entity instanceof BlockVariable) {
+            $entity->getBlock()->setUpdated(new \DateTime);   
+            
+            $entity->getBlock()->getPage()->setUpdated(new \DateTime);   
+            $entityManager->persist($entity->getBlock()->getPage());
+            $entityManager->flush();
+        }        
+        
+        if ($entity instanceof Block) {
+            if($entity->getPage()) {
+                $entity->getPage()->setUpdated(new \DateTime);   
+                $entityManager->persist($entity->getPage());
+                $entityManager->flush();                
+            }
         }
     }
 }
