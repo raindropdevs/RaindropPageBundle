81b48816bff132a9e1783a81d9e613676e6e3287
Edoardo Rivello <edoardo.rivello@gmail.com>
2013-11-08 17:44:28 +0100

    apply php cs

--- Listener/UpdateListener.php
+++ Listener/UpdateListener.php
@@ -1,11 +1,11 @@
 <?php
- 
+
 namespace Raindrop\PageBundle\Listener;
- 
+
 use Doctrine\ORM\Event\OnFlushEventArgs;
 use Raindrop\PageBundle\Entity\BlockVariable;
 use Raindrop\PageBundle\Entity\Block;
- 
+
 /**
  * UpdateListener
  */
@@ -20,35 +20,35 @@ class UpdateListener
     {
         $entityManager = $args->getEntityManager();
         $uow = $entityManager->getUnitOfWork();
- 
+
         $entities = array_merge(
             $uow->getScheduledEntityInsertions(),
             $uow->getScheduledEntityUpdates(),
             $uow->getScheduledEntityDeletions()
         );
- 
+
         foreach ($entities as $entity) {
-            
+
             if ($entity instanceof BlockVariable) {
                 $block = $this->getSuperBlock($entity->getBlock());
                 $page = $block->getPage();
- 
-                if($page) {
+
+                if ($page) {
                     $block->setUpdated(new \DateTime);
                     $page->setUpdated(new \DateTime);
                     $entityManager->persist($page);
 
                     $classMetadata = $entityManager->getClassMetadata(get_class($page));
-                    $uow->computeChangeSet($classMetadata, $page);                    
+                    $uow->computeChangeSet($classMetadata, $page);
                 }
             }
- 
+
             if ($entity instanceof Block) {
-                
+
                 $block = $this->getSuperBlock($entity);
-                
+
                 $page = $block->getPage();
-                
+
                 if ($page) {
                     $page->setUpdated(new \DateTime);
                     $entityManager->persist($page);
@@ -58,19 +58,19 @@ class UpdateListener
             }
         }
     }
-    
+
     /**
      * Return "main parent" block
-     * 
-     * @param type $block
+     *
+     * @param  type $block
      * @return type
      */
     private function getSuperBlock($block)
     {
-        while($block->getParent()) {
+        while ($block->getParent()) {
             $block = $block->getParent();
         }
-        
+
         return $block;
     }
 }
